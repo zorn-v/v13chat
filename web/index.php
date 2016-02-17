07c873e161bdcb6c6c8c39cb32e23e3a609db520
Zorn <zorn84@bk.ru>
2016-02-18 02:13:18 +1000

    Разделены env

--- web/index.php
+++ web/index.php
@@ -3,7 +3,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new App\Application();
-$app['debug'] = true;
 
 require_once __DIR__ . '/../config/services.php';
 require_once __DIR__ . '/../config/routes.php';
---
+++ web/index_dev.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new App\Application();
+$app['debug'] = true;
+
+require_once __DIR__ . '/../config/services.php';
+require_once __DIR__ . '/../config/routes.php';
+
+$app->run();
