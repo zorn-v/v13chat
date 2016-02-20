cbe04a1d57e5b8087957c7278d451d307c09f9c7
Zorn <zorn84@bk.ru>
2016-02-20 23:51:11 +1000

    Список онлайн юзеров на странице логина

--- app/Controller/Auth.php
+++ app/Controller/Auth.php
@@ -4,14 +4,21 @@ namespace App\Controller;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use App\Model\Session;
 
 class Auth
 {
     public function login(Application $app, Request $request)
     {
+        $onlineUsers = [];
+        $userSessions = Session::with('user')->get();
+        foreach ($userSessions as $session) {
+            $onlineUsers = $session->user->name;
+        }
         return $app['twig']->render('login.html.twig', array(
             'error'         => $app['security.last_error']($request),
             'last_username' => $app['session']->get('_security.last_username'),
+            'online_users'  => $onlineUsers
         ));
     }
 
