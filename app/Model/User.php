52c75a7cb9f1fe34851111bbeb53e981a1845b93
Zorn <zorn84@bk.ru>
2016-02-21 01:35:57 +1000

    Добавлены поля updated_at и created_at в модель User

--- app/Model/User.php
+++ app/Model/User.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    public $timestamps = false;
-
     public function role()
     {
         return $this->belongsTo('App\\Model\\Role');
