d3949d1f3112350a2a1b256ed7a99930a06a94de
Zorn <zorn84@bk.ru>
2016-03-09 23:51:36 +1000

    Форма отправки сообщения

--- app/Controller/Chat.php
+++ app/Controller/Chat.php
@@ -4,12 +4,41 @@ namespace App\Controller;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
-use App\Model\Config;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class Chat
 {
     public function layout(Application $app, Request $request)
     {
-        return $app['twig']->render('chat.html.twig');
+        $form = $app->form()
+            ->add('clear', ResetType::class)
+            ->add('to', TextType::class, ['required' => false])
+            ->add('message', TextType::class)
+            ->add('smiles_button', ButtonType::class)
+            ->add('send', SubmitType::class)
+            ->getForm();
+
+        if ($app->isGranted('ROLE_MODERATOR')) {
+            $choices = ['White (1)'=>'white', 'Pink (3)'=>'pink', 'Limegreen (3)'=>'limegreen'];
+            if ($app->isGranted('ROLE_REGISTRATOR')) {
+                $choices['Yellow (4)'] = 'yellow';
+                $choices['Red (4)'] = 'red';
+            }
+            if ($app->isGranted('ROLE_ADMIN')) {
+                $choices['Aqua (5)'] = 'aqua';
+            }
+            $form->add('color', ChoiceType::class, [
+                'choices' => $choices,
+                'choice_attr' => function($val, $key, $index) {
+                    return ['style' => 'background-color: '.$val];
+                },
+            ]);
+        }
+
+        return $app['twig']->render('chat.html.twig', ['chat_controls' => $form->createView()]);
     }
 }
