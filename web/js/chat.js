4c342da4de4bf85f5d037da8532d3a74e116686b
zorn <zorn84@bk.ru>
2016-03-10 12:00:15 +1000

    Добавление смайла в сообщение

--- web/js/chat.js
+++ web/js/chat.js
@@ -1,7 +1,13 @@
 jQuery(function ($) {
     var $smileBox = $('#smilebox');
     var $clearControls = $('.chat-controls .control-to, .chat-controls .control-message');
-    $('.chat-controls .control-smiles-button').click(function () {
+    var $message = $('.chat-controls .control-message');
+
+    $('body').click(function () {
+        $smileBox.hide();
+    });
+    $('.chat-controls .control-smiles-button').click(function (e) {
+        e.stopPropagation();
         $smileBox.toggle();
     });
     $('.chat-controls .control-color').change(function () {
@@ -9,5 +15,8 @@ jQuery(function ($) {
     }).change();
     $('.chat-controls .control-clear').click(function () {
         $clearControls.val('');
-    })
+    });
+    $('#smilebox img').click(function () {
+        $message.val($message.val() + ' ' + $(this).data('smile-text') + ' ');
+    });
 });
