e40adb0e4c44f297a95f19d46cc30d42f43187b9
Zorn <zorn84@bk.ru>
2016-07-28 21:16:12 +1000

    Реализованы kick/ban

--- app/Controller/Ajax.php
+++ app/Controller/Ajax.php
@@ -6,7 +6,9 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use App\Model\Session;
 use App\Model\Message;
+use App\Model\User;
 use App\Model\Ban;
+use App\Model\Log;
 
 class Ajax
 {
@@ -51,5 +53,41 @@ class Ajax
             Ban::where('user_id', $userId)->where('reason', Ban::REASON_SILENT)->delete();
         }
         return '';
+    }
+
+    public function userKick(Application $app, Request $request, $userId)
+    {
+        $user = User::find($userId);
+        $this->userBlock($app, $user, Ban::REASON_KICK, '+1 minute');
+        (new Message([
+            'message' => '<b><span style="color:red;">'.$app['user']->role->description.'</span></b>: '.
+                         'пользователь с ником <span style="color:red;">'.$user->name.'</span> получает предупреждение!'
+        ]))->save();
+        return '';
+    }
+
+    public function userBan(Application $app, Request $request, $userId)
+    {
+        $user = User::find($userId);
+        $this->userBlock($app, $user, Ban::REASON_BAN, '+90 minute');
+        (new Message([
+            'message' => '<b><span style="color:red;">'.$app['user']->role->description.'</span></b>: '.
+                         'пользователь с ником <span style="color:red;">'.$user->name.'</span>  удален из чата!'
+        ]))->save();
+        return '';
+    }
+
+    private function userBlock(Application $app, User $user, $reason, $blockUntil)
+    {
+        if ($app->isGranted('ROLE_MODERATOR') && $app->isGranted('CAN_BAN', $user)) {
+            Session::where('user_id', $user->id)->delete();
+            $blockTime = new \DateTime($blockUntil);
+            $ban = new Ban();
+            $ban->user_id = $user->id;
+            $ban->author_id = $app['user']->id;
+            $ban->reason = $reason;
+            $ban->until = $blockTime;
+            $ban->save();
+        }
     }
 }
--- config/routes.php
+++ config/routes.php
@@ -9,3 +9,5 @@ $app->post('/users-list', 'App\\Controller\\Ajax::usersList')->bind('users-list'
 $app->post('/messages', 'App\\Controller\\Ajax::messages')->bind('messages');
 $app->post('/message-delete/{messageId}', 'App\\Controller\\Ajax::messageDelete')->bind('message-delete');
 $app->post('/user-voice/{userId}', 'App\\Controller\\Ajax::userVoice')->bind('user-voice');
+$app->post('/user-kick/{userId}', 'App\\Controller\\Ajax::userKick')->bind('user-kick');
+$app->post('/user-ban/{userId}', 'App\\Controller\\Ajax::userBan')->bind('user-ban');
